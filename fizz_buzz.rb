dcdf66e984f27990b4327d0c2e7eeec74f197e03
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-05-26 08:27:40 +0900

    first fizz buzz

---
+++ fizz_buzz.rb
@@ -0,0 +1,20 @@
+def fizz_buzz(n)
+  if (n % 15).zero?
+    'Fizz Buzz'
+  elsif (n % 3).zero?
+    'Fizz'
+  elsif (n % 5).zero?
+    'Buzz'
+  else
+    n.to_s
+  end
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(15)
