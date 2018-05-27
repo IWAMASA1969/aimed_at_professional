6a484dec24f23e2d54583d5cc773a598ee6120e8
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-05-27 17:50:28 +0900

    fixed cnvert length

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,4 @@
-def convert_length(l, s, d)
-  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
-  (l / units[s] * units[d]).round(2)
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }.freeze
+def convert_length(l, from: :m, to: :m)
+  (l / UNITS[from] * UNITS[to]).round(2)
 end
